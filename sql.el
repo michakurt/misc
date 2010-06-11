b6be5aadc355b26a1792c8bcc608fb5e74797efc
michakurt <michakurt@gmail.com>
2010-06-11 18:37:04 +0200

    geht ganz gut!

--- sql.el
+++ sql.el
@@ -15,10 +15,11 @@
   (progn
     (with-buffer "sql"
       (progn
-	(insert "set lin 32767 tab off emb on pages 0 newp 0 head on sqlp 'SQL> '")
+	(insert "set feed on lin 32767 tab off emb on pages 0 newp 0 head on sqlp 'SQL> '")
 	(comint-send-input)
 	(erase-buffer)
 	(insert sql)
+	(insert "\n")
 	(setq b (point))
 	(comint-send-input)
 
@@ -69,5 +70,34 @@ also:
 (defun sqlplus-execute-command-at-point ()
   "nimmt das sql-Kommando entgegen. Wenn es ein select ist, dann dann an sql-send-select uebergeben."
   (interactive)
-  (sql-send-select (buffer-substring-no-properties 1 (point-max))))
+  (let ((x (sqlplus-mark-current)))
+    (sql-send-select (sqlplus-remove-linebreaks (buffer-substring-no-properties (car x) (cdr x))))))
+
+(defun sqlplus-mark-current ()
+  "Marks the current SQL for sending to the SQL*Plus process.  Marks are placed around a region defined by empty lines."
+  (let (begin end empty-line-p empty-line-p next-line-included tail-p)
+    (save-excursion
+      (beginning-of-line)
+      (setq empty-line-p (when (looking-at "^[ \t]*\\(\n\\|\\'\\)") (point)))
+      (setq next-line-included (and empty-line-p (save-excursion (skip-chars-forward " \t\n") (> (current-column) 0))))
+      (setq tail-p (and empty-line-p
+			(or (bobp) (save-excursion (beginning-of-line 0) (looking-at "^[ \t]*\n"))))))
+    (unless tail-p
+      (save-excursion
+	(end-of-line)
+	(re-search-backward "\\`\\|\n[\r\t ]*\n[^ \t]" nil t)
+	(skip-syntax-forward "-")
+	(setq begin (point)))
+      (save-excursion
+	(beginning-of-line)
+	(re-search-forward "\n[\r\t ]*\n[^ \t]\\|\\'" nil t)
+	(unless (zerop (length (match-string 0)))
+	  (backward-char 1))
+	(skip-syntax-backward "-")
+	(setq end (or (and (not next-line-included) empty-line-p) (point)))))
+    (cons begin end)))
+
+(defun sqlplus-remove-linebreaks (txt)
+  (replace-regexp-in-string "\n" " " txt))
+
 
