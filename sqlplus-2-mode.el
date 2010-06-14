6300a496cc2d085be5f551eb2dbadc888aab37c2
michakurt <michakurt@gmail.com>
2010-06-14 06:31:38 +0200

    *** empty log message ***

--- sqlplus-2-mode.el
+++ sqlplus-2-mode.el
@@ -15,20 +15,19 @@
 
 (defvar sqlplus-2-interrupted nil)
 
-(define-derived-mode sqlplus-2-mode fundamental-mode "sqlplus-2"
+(define-derived-mode sqlplus-2-mode sql-mode "sqlplus-2"
    "Major mode to edit sql and send it to sqlplus. Does formatting on output of select statements"
    (use-local-map sqlplus-2-mode-map)
-   (set (make-local-variable 'font-lock-keywords)
-        '(sqlplus-2-mode-font-lock-keywords))
-   (set (make-local-variable 'comment-start) "--"))
-
+;   (set (make-local-variable 'font-lock-keywords)
+;        '(sqlplus-2-mode-font-lock-keywords))
+;   (set (make-local-variable 'comment-start) "--"))
+)
 
 
 (defun sqlplus-2-chomp (str)
   "Chomp leading and tailing whitespace from STR."
   (let ((s (if (symbolp str) (symbol-name str) str)))
     (replace-regexp-in-string "\\(^[[:space:]\\n]*\\|[[:space:]\\n]*$\\)" "" s)))
-
 
 (defun sqlplus-2-trim-trailing-whitespace (str)
   (string-match "^\\(.*\\S-\\)\\s-*$" str )
@@ -39,14 +38,11 @@
       (sqlplus-2-fill-with-blank (concat s " ") l)
     s
     ))
-  
 
 (defun sqlplus-2-split-string-to-cols (s l)
   (if (cdr l)
       (cons (substring s 0 (car l)) (sqlplus-2-split-string-to-cols (substring s (+ 1 (car l))) (cdr l)))
     (list s) ))
-
-
 
 (defun sqlplus-2-trimmax (l)
   (let* ((ltrimmed (mapcar 'sqlplus-2-chomp l))
@@ -57,7 +53,6 @@
   (if (not (car l)) nil
     (cons (mapcar (lambda (q) (car q)) l) (sqlplus-2-transpose (mapcar (lambda (q) (cdr q)) l)))))
 
-
 (defun sqlplus-2-split-string-to-cols (s l)
   (cons (substring (sqlplus-2-fill-with-blank s (car l)) 0 (car l))
 	(if (cdr l)
@@ -65,27 +60,25 @@
 	  nil)))
 
 (defun sqlplus-2-parse-select-output- (txt)
-  (let* (
-	 (ol (split-string txt "\n" t))
+  (let* ((ol (split-string txt "\n" t))
 	 (colnames (car ol))
 	 (dashes (car (cdr ol)))
 	 (cols-lengths (mapcar 'length (split-string dashes)))
 	 (data (cons colnames (cdr (cdr ol))))
-	 (normalized-data (mapcar (lambda (q) (sqlplus-2-split-string-to-cols q cols-lengths)) data))
-	 )
+	 (normalized-data (mapcar (lambda (q) (sqlplus-2-split-string-to-cols q cols-lengths)) data)))
     normalized-data))
 
-    
 (defun sqlplus-2-normalize-select-output (txt)
   (sqlplus-2-transpose (mapcar 'sqlplus-2-trimmax (sqlplus-2-transpose (sqlplus-2-parse-select-output- txt) ))))
 
 (defun sqlplus-2-is-sql-prompt-in-current-buffer ()
-  (equal "SQL> " (buffer-substring-no-properties (- (point-max) 5) (point-max))))
+  (and (>= (- (point-max) (point-min)) 5)
+       (equal "SQL> " (buffer-substring-no-properties (- (point-max) 5) (point-max)))))
 
 (defun sqlplus-2-ensure-sql-prompt (buf)
   (with-buffer buf
     (if (sqlplus-2-is-sql-prompt-in-current-buffer) buf
-      (error "Please start sqlplus in this buffer"))))
+      (error "Please start sqlplus in this buffer and retry."))))
 
 (defun sqlplus-2-get-or-create-output-buffer ()
     (get-buffer-create sqlplus-2-output-buffer-name))
@@ -93,7 +86,6 @@
 (defun sqlplus-2-get-or-create-interaction-buffer ()
   (if sqlplus-2-hidden-sqlplus-interaction-buffer sqlplus-2-hidden-sqlplus-interaction-buffer
     (shell (get-buffer-create sqlplus-2-hidden-sqlplus-interaction-buffer-name))))
-
 
 (defun sqlplus-2-command-to-sqlplus (buf cmd)
   (let* ((process (get-buffer-process buf)))
@@ -122,33 +114,24 @@
   (if (equal "select" (substring s 0 6)) (concat "select * from (" (replace-regexp-in-string "[; ]+$" "" s) ") where rownum < 20;")
     s))
 
-(defun sqlplus-2-send-select (sql)
+(defun sqlplus-2-send-statement (sql)
   (interactive)
   (let* ((interaction-buffer (sqlplus-2-ensure-sql-prompt (sqlplus-2-get-or-create-interaction-buffer)))
 	(output-buffer (sqlplus-2-get-or-create-output-buffer)))
     (with-buffer interaction-buffer
       (progn
-;	(goto-char 1)
 	(erase-buffer)
 	(insert "set feed on lin 32767 tab off emb on pages 0 newp 0 head on sqlp 'SQL> '\n")
 	(comint-send-input)
 	(sqlplus-2-wait-for-prompt 12)
 	(erase-buffer)
 	(insert (sqlplus-2-wrap-select-limit-lines sql))
-;	(insert "\n")
 	(setq b (point))
 	(comint-send-input)
-;	(sleep 3)
 	(sqlplus-2-wait-for-prompt 12)
-	(goto-char (point-max))
 
-;; 	(while (progn
-;; 		 (if sqlplus-2-interrupted (error "Interrupt")
-;; 		   (progn
-;; 		     (goto-char b)
-;; 		     (sit-for 1)
-;; 		     (not (sqlplus-2-is-sql-prompt-in-current-buffer))))))
-	  
+	;If the output is from a  select statement, we call sqlplus-2-normalize-select-output on it and write
+	;the result to the output-buffer. Otherwise we write the plain-result from sqlplus to the output-buffer
 	(goto-char 1)
 	(if (re-search-forward "\\([0-9]+\\) row[s]* selected." nil t)
 	    (progn
@@ -175,8 +158,6 @@
 		(erase-buffer)
 		(insert x)))))))))
 
-
-
 (defun sqlplus-2-remove-linebreaks (txt)
   (replace-regexp-in-string "\n" " " txt))
 
@@ -185,14 +166,14 @@
    (progn
      (goto-char 1)
      (end-of-line)
-     (put-text-property 1 (point)  'face '(:foreground "yellow")))))
+     (put-text-property 1 (point) 'face '(:foreground "yellow")))))
     
 (defun sqlplus-2-process ()
-  "nimmt das sql-Kommando entgegen. Wenn es ein select ist, dann dann an sql-send-select uebergeben."
+  "nimmt das sql-Kommando entgegen."
   (interactive)
   (let ((x (sqlplus-2-mark-current))
 	(cb (current-buffer)))
-    (sqlplus-2-send-select (sqlplus-2-remove-linebreaks (buffer-substring-no-properties (car x) (cdr x))))
+    (sqlplus-2-send-statement (sqlplus-2-remove-linebreaks (buffer-substring-no-properties (car x) (cdr x))))
     (switch-to-buffer-other-window cb)
     (switch-to-buffer-other-window (sqlplus-2-get-or-create-output-buffer))
     (switch-to-buffer-other-window cb)))
